cb511c92841d5d3d05506b860b8866723421046a
1310jessicagrace <jessicagracea024@gmail.com>
2025-05-29 11:46:42 +0530

    Home

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,17 +2,14 @@ import { Link } from "react-router";
 
 export default function Home() {
   return (
-    <>
-      <section className="flex flex-col items-center justify-center min-h-[80vh] px-4">
-        <div className="text-gray-800 text-3xl font-bold mb-4">
-          Welcome to My Application
-        </div>
+    <div>
+      <section className="flex flex-col items-center  min-h-[80vh] px-4">
         <button className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded-lg shadow-lg text-lg transition duration-300">
-          <Link to={'/SignIn'}>
-          Get Started
+          <Link to={'/login'}>
+          Adopt a pet now!
           </Link>
         </button>
       </section>
-    </>
+    </div>
   );
 }
