e2ad83bc3e82b1b8cd6f3073490ca2b5b4eedf26
Dharani <e.dharani2468@gmail.com>
2025-05-29 11:56:53 +0530

    Approutes

--- client/src/routes/AppRoute.jsx
+++ client/src/routes/AppRoute.jsx
@@ -1,43 +1,21 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
-import Login from "../pages/SignIn";
-import SignUp from "../pages/SignUp";
-// import NotFound from "../pages/NotFound";
-import BasicLayouts from "../layouts/BasicLayouts";
-import DashboardLayouts from "../layouts/DashboardLayouts";
-// import Intro from "../pages/dashboard/Intro";
-// import Counter from "../pages/dashboard/Counter";
-// import Calculator from "../pages/dashboard/Calculator";
-// import TodoList from "../pages/dashboard/TodoList";
-// import Profile from "../pages/dashboard/Profile";
-import PrivateRoute from "./PrivateRoute";
-// import StopWatch from "../pages/dashboard/StopWatch";
+// src/routes/AppRoutes.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Developer from '../pages/Developer';
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<BasicLayouts />}>
-        <Route index element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/SignIn" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Route>
-      <Route
-        path="/dashboard"
-        element={(
-          <PrivateRoute>
-            <DashboardLayouts />
-          </PrivateRoute>)}
-      >
-        {/* <Route path="/dashboard/intro" element={<Intro />} />
-        <Route path="/dashboard/counter" element={<Counter />} />
-        <Route path="/dashboard/calc" element={<Calculator />} />
-        <Route path="/dashboard/todo-list" element={<TodoList />} />
-        <Route path="/dashboard/profile" element={<Profile />} />
-        <Route path="/dashboard/stop-watch" element={<StopWatch />} /> */}
-      </Route>
-
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/developer" element={<Developer />} />
+      </Routes>
+    </Router>
+  );
 }
